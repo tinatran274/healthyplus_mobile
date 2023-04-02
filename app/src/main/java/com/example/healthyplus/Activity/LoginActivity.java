5631eb1017bd0595215a939d078bcf7fcae4f766
Nguyễn Sỹ Lê Hoàng <21520870@gm.uit.edu.vn>
2023-04-03 05:29:03 +0700

    add Login Activity

---
+++ app/src/main/java/com/example/healthyplus/Activity/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.healthyplus.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+
+import com.example.healthyplus.R;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+
+}
+
